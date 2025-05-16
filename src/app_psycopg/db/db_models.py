7082d8233ab54c368757e2c7f6c1303d403f9b4b
johschmidt42 <johannes.schmidt.vik@gmail.com>
2025-05-16 22:28:38 +0200

    forget fields in db models

--- src/app_psycopg/db/db_models.py
+++ src/app_psycopg/db/db_models.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class User(BaseModel):
     id: str
     name: str
+    created_at: datetime
+    last_updated_at: Optional[datetime] = None
 
 
 class Order(BaseModel):
@@ -16,3 +21,5 @@ class Order(BaseModel):
 class Document(BaseModel):
     id: str
     document: dict
+    created_at: datetime
+    last_updated_at: Optional[datetime] = None
