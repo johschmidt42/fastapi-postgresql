98953af2c5efce50f0cee8405404a28b42da89f6
johschmidt42 <johannes.schmidt.vik@gmail.com>
2025-05-18 21:36:27 +0200

    constraints

--- src/app_psycopg/api/models.py
+++ src/app_psycopg/api/models.py
@@ -1,16 +1,30 @@
 import json
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Annotated, Type
 from uuid import uuid4
 
-from pydantic import BaseModel, computed_field, ConfigDict, field_serializer
+from pydantic import (
+    BaseModel,
+    computed_field,
+    ConfigDict,
+    field_serializer,
+    StringConstraints,
+    Field,
+)
 
 
 # User
 
+UserName: Type = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True, to_upper=True, min_length=1, max_length=20
+    ),
+]
+
 
 class UserInput(BaseModel):
-    name: str
+    name: UserName
 
     @computed_field
     def id(self) -> str:
@@ -22,7 +36,7 @@ class UserInput(BaseModel):
 
 
 class UserUpdate(BaseModel):
-    name: str
+    name: UserName
 
     @computed_field
     def last_updated_at(self) -> datetime:
@@ -31,7 +45,7 @@ class UserUpdate(BaseModel):
 
 class UserResponseModel(BaseModel):
     id: str
-    name: str
+    name: UserName
     created_at: datetime
     last_updated_at: Optional[datetime] = None
 
@@ -40,9 +54,13 @@ class UserResponseModel(BaseModel):
 
 # Order
 
+OrderAmount: Type = Annotated[
+    float, Field(strict=True, gt=0, le=1_000_000, decimal_places=2)
+]
+
 
 class OrderInput(BaseModel):
-    amount: float
+    amount: OrderAmount
     payer_id: str
     payee_id: str
 
@@ -53,7 +71,7 @@ class OrderInput(BaseModel):
 
 class OrderResponseModel(BaseModel):
     id: str
-    amount: float
+    amount: OrderAmount
     payer: UserResponseModel
     payee: UserResponseModel
 
@@ -62,9 +80,11 @@ class OrderResponseModel(BaseModel):
 
 # Document
 
+NonEmptyDict: Type = Annotated[dict, Field(min_length=1)]
+
 
 class DocumentInput(BaseModel):
-    document: dict
+    document: NonEmptyDict
 
     @computed_field
     def id(self) -> str:
@@ -80,7 +100,7 @@ class DocumentInput(BaseModel):
 
 
 class DocumentUpdate(BaseModel):
-    document: dict
+    document: NonEmptyDict
 
     @computed_field
     def last_updated_at(self) -> datetime:
@@ -93,7 +113,7 @@ class DocumentUpdate(BaseModel):
 
 class DocumentResponseModel(BaseModel):
     id: str
-    document: dict
+    document: NonEmptyDict
     created_at: datetime
     last_updated_at: Optional[datetime] = None
 
--- src/app_sqlalchemy/api/models.py
+++ src/app_sqlalchemy/api/models.py
@@ -1,12 +1,21 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Annotated, Type
 from uuid import uuid4
 
-from pydantic import BaseModel, computed_field, ConfigDict
+from pydantic import BaseModel, computed_field, ConfigDict, StringConstraints, Field
+
+# User
+
+UserName: Type = Annotated[
+    str,
+    StringConstraints(
+        strip_whitespace=True, to_upper=True, min_length=1, max_length=20
+    ),
+]
 
 
 class UserInput(BaseModel):
-    name: str
+    name: UserName
 
     @computed_field
     def id(self) -> str:
@@ -18,7 +27,7 @@ class UserInput(BaseModel):
 
 
 class UserUpdate(BaseModel):
-    name: str
+    name: UserName
 
     @computed_field
     def last_updated_at(self) -> datetime:
@@ -27,15 +36,22 @@ class UserUpdate(BaseModel):
 
 class UserResponseModel(BaseModel):
     id: str
-    name: str
+    name: UserName
     created_at: datetime
     last_updated_at: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
 
 
+# Order
+
+OrderAmount: Type = Annotated[
+    float, Field(strict=True, gt=0, le=1_000_000, decimal_places=2)
+]
+
+
 class OrderInput(BaseModel):
-    amount: float
+    amount: OrderAmount
     payer_id: str
     payee_id: str
 
@@ -46,15 +62,20 @@ class OrderInput(BaseModel):
 
 class OrderResponseModel(BaseModel):
     id: str
-    amount: float
+    amount: OrderAmount
     payer: UserResponseModel
     payee: UserResponseModel
 
     model_config = ConfigDict(from_attributes=True)
 
 
+# Document
+
+NonEmptyDict: Type = Annotated[dict, Field(min_length=1)]
+
+
 class DocumentInput(BaseModel):
-    document: dict
+    document: NonEmptyDict
 
     @computed_field
     def id(self) -> str:
@@ -66,7 +87,7 @@ class DocumentInput(BaseModel):
 
 
 class DocumentUpdate(BaseModel):
-    document: dict
+    document: NonEmptyDict
 
     @computed_field
     def last_updated_at(self) -> datetime:
@@ -75,7 +96,7 @@ class DocumentUpdate(BaseModel):
 
 class DocumentResponseModel(BaseModel):
     id: str
-    document: dict
+    document: NonEmptyDict
     created_at: datetime
     last_updated_at: Optional[datetime] = None
 
