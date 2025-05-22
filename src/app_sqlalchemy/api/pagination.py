235499a0626a2d9febab6e41b59b7f952249a6b9
johschmidt42 <johannes.schmidt.vik@gmail.com>
2025-05-22 16:18:50 +0200

    create_paginate_query

--- src/app_sqlalchemy/api/pagination.py
+++ src/app_sqlalchemy/api/pagination.py
@@ -1,10 +1,7 @@
-from typing import Generic, TypeVar, Sequence, LiteralString
+from typing import Generic, TypeVar, Sequence
 
-from psycopg.abc import Query
 from pydantic import conint, BaseModel
-
-from psycopg import sql
-
+from sqlalchemy import Select
 
 DataT: TypeVar = TypeVar("DataT")
 
@@ -17,16 +14,20 @@ class LimitOffsetPage(BaseModel, Generic[DataT]):
     offset: conint(ge=0, le=1000)
 
 
-def create_paginate_query(query: Query, limit: int, offset: int) -> Query:
-    suffix: LiteralString = f" LIMIT {limit} OFFSET {offset}"
+def create_paginate_query(query: Select, limit: int, offset: int) -> Select:
+    """
+    Applies LIMIT and OFFSET clauses to a SQLAlchemy Select query.
 
-    if isinstance(query, bytes):
-        return query + suffix.encode()
-    elif isinstance(query, (sql.SQL, sql.Composed)):
-        return sql.Composed([query, sql.SQL(suffix)])
-    elif isinstance(query, str):
-        return f"{query}{suffix}"
-    else:
-        raise TypeError(
-            "Query must be a LiteralString, bytes, sql.SQL, or sql.Composed"
-        )
+    Args:
+        query: The SQLAlchemy Select statement object.
+        limit: The maximum number of rows to return (LIMIT).
+        offset: The number of rows to skip before starting to return rows (OFFSET).
+
+    Returns:
+        A new SQLAlchemy Select statement object with LIMIT and OFFSET applied.
+    """
+
+    # Apply limit and offset to the query
+    paginated_query: Select = query.limit(limit).offset(offset)
+
+    return paginated_query
