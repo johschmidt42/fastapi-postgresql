4a5c76ad965f97af9f72d10757c3bbcc80234271
johschmidt42 <johannes.schmidt.vik@gmail.com>
2025-05-22 16:08:52 +0200

    plural routes

--- src/app_psycopg/api/app.py
+++ src/app_psycopg/api/app.py
@@ -6,8 +6,8 @@ from app_psycopg.api.lifespan import lifespan
 from app_psycopg.api.routes import users
 from app_psycopg.api.routes import orders
 from app_psycopg.api.routes import documents
-from app_psycopg.api.routes import profession
-from app_psycopg.api.routes import company
+from app_psycopg.api.routes import professions
+from app_psycopg.api.routes import companies
 from app_psycopg.api.routes import user_company_links
 
 app: FastAPI = FastAPI(lifespan=lifespan)
@@ -15,8 +15,8 @@ app: FastAPI = FastAPI(lifespan=lifespan)
 app.include_router(router=users.router)
 app.include_router(router=orders.router)
 app.include_router(router=documents.router)
-app.include_router(router=profession.router)
-app.include_router(router=company.router)
+app.include_router(router=professions.router)
+app.include_router(router=companies.router)
 app.include_router(router=user_company_links.router)
 
 if __name__ == "__main__":  # pragma: no cover
